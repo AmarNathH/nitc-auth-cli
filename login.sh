735437d639a456f613bed103034e2cc48913ae3b
AmarNathH <amarnath.h.96@gmail.com>
2019-03-11 18:10:09 +0530

    Improved login code, and code fixes in logout script

--- login.sh
+++ login.sh
@@ -35,7 +35,7 @@ echo $secure_key >> log_file
 # curl "${web_address}" -H 'Connection: keep-alive' -H 'Cache-Control: max-age=0' -H "Origin: $web_address" -H 'Upgrade-Insecure-Requests: 1' -H 'Content-Type: application/x-www-form-urlencoded' -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8' -H "Referer: $web_address/fgtauth?$secure_key" -H 'Accept-Encoding: gzip, deflate' -H 'Accept-Language: en-GB,en-US;q=0.9,en;q=0.8' --data "4Tredir=http%3A%2F%2Fwww.gstatic.com%2Fgenerate_204&magic=$secure_key&username=$username&password=$password" --compressed
 
 #reduced form
-checkoutput=$(curl -s "${web_address}" -H "Origin: $web_address" -H "Referer: $web_address/fgtauth?$secure_key" --data "4Tredir=http%3A%2F%2Fwww.gstatic.com%2Fgenerate_204&magic=$secure_key&username=$username&password=$password" --compressed)
+checkoutput=$(curl -s "${web_address}" -H 'Connection: keep-alive' -H "Origin: $web_address" -H "Referer: $web_address/fgtauth?$secure_key" --data "4Tredir=http%3A%2F%2Fwww.gstatic.com%2Fgenerate_204&magic=$secure_key&username=$username&password=$password" --compressed)
 
 #extract status from the output of curl
 checkoutput=$(echo "$checkoutput" | sed -n "75p")
--- logout.sh
+++ logout.sh
@@ -12,6 +12,11 @@ secure_key=$(cat log_file | sed -n '2p')
 #curl 'http://192.168.102.1:1000/logout?0d080d000a135b5c' -H 'Connection: keep-alive' -H 'Upgrade-Insecure-Requests: 1' -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8' -H 'Referer: http://192.168.102.1:1000/keepalive?0d080d000a135b5c' -H 'Accept-Encoding: gzip, deflate' -H 'Accept-Language: en-GB,en-US;q=0.9,en;q=0.8' --compressed
 
 #reduced form
-curl "$web_address/logout?$secure_key" -H "Referer: $web_address/keepalive?$secure_key" --compressed
-# !!Dev: Give status of the logout whether it was successful or not
-echo "Logged out from: $web_address with key: $secure_key"
+checkoutput=$(curl -s --connect-timeout 3.0 "$web_address/logout?$secure_key" -H "Referer: $web_address/keepalive?$secure_key" --compressed)
+
+if [ "$checkoutput" == "" ]; then
+    echo "Something is not right please check the log_file." 
+    echo "If the IP address is not correct, either change it to correct one or rerun login.sh script"
+else 
+    echo "Logout Successful"
+fi
